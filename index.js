407e479fb117e38efa9eebc06f7d5798e35cfceb
Martijn Michel <info@tocadovision.nl>
2021-02-13 09:32:08 +0100

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const { CommentStream } = require("snoostorm");
+
+require("dotenv").config();
+const Snoowrap = require("snoowrap");
+const Snoostorm = require("snoostorm");
+
+const r = new Snoowrap({
+  userAgent: "some-description",
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  username: process.env.REDDIT_USER,
+  password: process.env.REDDIT_PASS,
+});
+
+const stream = new CommentStream(r, {
+  subreddit: "testingground4bots",
+  results: 25,
+});
+
+stream.on("item", (comment) => {
+  console.log(comment);
+  if (comment.body === "sonid is so cool!") {
+    comment.reply(":)");
+  }
+});
